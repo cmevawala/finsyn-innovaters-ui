3a4dfdd22353e517e0e44ffae4f2f1e996482143
cmevawala <chintu.m18@gmail.com>
2025-02-19 21:59:59 +0530

    added drift report

--- src/components/PrompInputElement/PromptInputElement.jsx
+++ src/components/PrompInputElement/PromptInputElement.jsx
@@ -1,20 +1,42 @@
 
 import React, { useState, useEffect } from "react";
-import { Container, TextField, Button, Box } from "@mui/material";
+import { Container, TextField, Button, Box, Typography } from "@mui/material";
 import Loader from "../Loader";
 import { useStateContext } from "../PromptSyntheticDataGenerator/StateContext";
 
 function PromptInputElement() {
     const { state, dispatch } = useStateContext();
-    const { schema = {} } = state;
+    const { schema = {}, syntheticData = [] } = state;
 
     const [text, setText] = useState("");
     const [loading, setLoading] = useState(false);
 
+    const fetchSytheticDataFromMetadata = async () => {
+        try {
+            const res = await fetch("http://localhost:5000/synthetic_data_from_metadata", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                //   body: JSON.stringify({ prompt: text }),
+            });
+            const data = await res.json();
+            if (data) {
+                dispatch({ type: "SYNTHETIC_DATA_FROM_METADATA", payload: data });
+            }
+        } catch (error) {
+            console.error("Error calling API:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchSytheticDataFromMetadata();
+    }, [])
+
     // useEffect(() => {
-    //     // fetch("http://localhost:5000/response_get_schema_from_users_prompt")
-    //     //     .then(response => response.json())
-    //     //     .then(data => console.log(data));
+    //     fetch("http://localhost:5000/synthetic_data_from_metadata")
+    //         .then(response => response.json())
+    //         .then(data => console.log(data));
     // }, []); 
 
     const fetchSchema = async () => {
@@ -48,14 +70,20 @@ function PromptInputElement() {
         return <Loader />
     }
 
-    if (schema !== "") return <></>
+    if (schema.length !== 0) return <></>
 
     return (
         <Box sx={{
-            color: 'white',
             p: 2,  // Padding of 2 units (16px)
             borderRadius: 2 // Rounded corners
         }}>
+
+            {/* <h2>Generate Metadata</h2> */}
+
+            <Typography variant="h5" gutterBottom sx={{ mb: 2 }}>
+                Synthetic Data Report
+            </Typography>
+
             {/* Text Area */}
             <TextField
                 multiline
