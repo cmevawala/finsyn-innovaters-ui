0bd04d6be971d8ce3e0f292764bcecb187c6889e
cmevawala <chintu.m18@gmail.com>
2025-02-19 11:49:03 +0530

    created mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +6,12 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
+  
   return (
     <>
       <div>
